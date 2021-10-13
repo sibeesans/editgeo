d6d3196ce2bb1b58d71d1df348f13f3d068a8292
geogabut <76270969+geogabut@users.noreply.github.com>
2021-10-13 10:41:08 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+figlet "GEO GABUT"
+figlet "PREMIUM SCRIPT"
+echo -e "=================================================" | lolcat
+echo -e "#             AutoScript VPS                    #"
+echo -e "#-----------------------------------------------#"
+echo -e "#             OPERATING SYSTEM                  #"
+echo -e "             -------------------                 "
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "=================================================" | lolcat
