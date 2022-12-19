2ac5731a66df2b627ecd29099087ce37c28f71ae
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-19 23:00:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,6 @@ BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo -e  "${RED}Checking VPS${NC}"
-sleep 2
-IZIN=$(curl https://raw.githubusercontent.com/sibeesans/DorkScript/main/ipvps | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "${GREEN}Permission Accepted...${NC}"
 sleep 2 
 else
 clear
